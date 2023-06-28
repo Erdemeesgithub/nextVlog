544e5e9664e5364494450b08ed75387a0df24dc6
Erdem <berdemdelgerekh@gmail.com>
2023-06-28 12:37:03 +0800

    deployed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Card, CardContent } from "@mui/joy";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 
 export default function Home() {
   const [articles, setArticles] = useState([]);
@@ -15,6 +16,13 @@ export default function Home() {
   return (
     <>
       <div>
+        <Head>
+          <meta property="og:title" content="Join us on Erdem's Vlog post ;)" />
+          <meta
+            property="og:image"
+            content="https://i.pinimg.com/564x/5c/b0/2a/5cb02a70ecc01536cb4d5579ef6f2379.jpg"
+          />
+        </Head>
         {articles.map((article) => (
           <Card key={article.id} color="success" variant="outlined">
             <CardContent>
