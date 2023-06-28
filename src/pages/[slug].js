98479d8fd9eb351ab0e51de1023eb984b4680dd9
Erdem <berdemdelgerekh@gmail.com>
2023-06-28 13:35:12 +0800

    meta is done

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -1,31 +1,27 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import Head from "next/head";
+import Typography from "@mui/joy/Typography";
 
-export default function App() {
-  const router = useRouter();
-
-  const { slug } = router.query;
-
-  const [article, setArticle] = useState();
-
-  useEffect(() => {
-    if (slug) {
-      fetch(`https://dev.to/api/articles/whitep4nth3r/${slug}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setArticle(data);
-        });
-    }
-  }, [slug]);
-
-  console.log({ article });
-
-  if (!article) return <div>Loading...</div>;
+export default function App({ article }) {
+  if (!article) return <div>Not found...</div>;
 
   return (
     <>
-      <h1>{article.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: article.body_html }} />
+      <Head>
+        <meta property="og:title" content={article.title} />
+        <meta property="og:image" content={article.social_image} />
+        <meta property="og:description" content={article.description} />
+      </Head>
+      <Typography component="div">
+        <h1>{article.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: article.body_html }} />
+      </Typography>
     </>
   );
+}
+
+export async function getServerSideProps({ params }) {
+  const { slug } = params;
+  const res = await fetch(`https://dev.to/api/articles/whitep4nth3r/${slug}`);
+  const data = await res.json();
+  return { props: { article: data } };
 }
